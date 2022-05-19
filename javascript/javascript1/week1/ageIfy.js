169d3d6919b4237b53ccfd9b0184a0ab878cb071
Rostyslav Zhyvitskyi <rostik94@gmail.com>
2022-05-19 06:07:32 +0200

    error handeling added

--- javascript/javascript1/week1/ageIfy.js
+++ javascript/javascript1/week1/ageIfy.js
@@ -2,12 +2,30 @@ const yearOfBirthInput = document.getElementById('year-of-birth');
 const futureYearInput = document.getElementById('future-year');
 const calculateYearBtn = document.getElementById('calculate-age-ify');
 const futureAgeResult = document.getElementById('future-year-result');
+const birthYearErrorHandler = document.getElementById('age-error');
+const futureYearErrorHandler = document.getElementById('future-error');
 
 calculateYearBtn.addEventListener('click', (event) => {
   event.preventDefault();
+  birthYearErrorHandler.textContent = '';
+  futureYearErrorHandler.textContent = '';
+  yearOfBirthInput.classList.remove('error-border');
+  futureYearInput.classList.remove('error-border');
 
   const yearOfBirth = yearOfBirthInput.value;
   const futureYear = futureYearInput.value;
+
+  console.log(yearOfBirth, futureYear);
+
+  if (yearOfBirth === '') {
+    birthYearErrorHandler.textContent = 'Please enter your real birth year';
+    yearOfBirthInput.classList.add('error-border');
+    return;
+  } else if (futureYear === '') {
+    futureYearErrorHandler.textContent = 'Please enter future year';
+    futureYearInput.classList.add('error-border');
+    return;
+  }
 
   const futureAge = futureYear - yearOfBirth;
 
