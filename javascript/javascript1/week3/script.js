a7369768259547c06eab4ea8ab4486b5569ba059
Rostyslav Zhyvitskyi <rostik94@gmail.com>
2022-05-30 08:49:28 +0200

    changing related to the feedback

--- javascript/javascript1/week3/script.js
+++ javascript/javascript1/week3/script.js
@@ -1,6 +1,6 @@
 // Item array removal =================================================================================
 
-const names = [
+const namesArray = [
   'Peter',
   'Ahmad',
   'Yana',
@@ -18,9 +18,9 @@ const removeName = (name, nameArray) => {
   nameArray.splice(indexOfNameToRemove, 1);
 };
 
-removeName(nameToRemove, names);
+removeName(nameToRemove, namesArray);
 
-console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+console.log(namesArray); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
 
 // When will we be there?? ========================================================================
 
@@ -30,9 +30,9 @@ const travelInformation = {
 };
 
 const getTravelTime = ({ speed, destinationDistance }) => {
-  const totalTime = (destinationDistance / speed) * 60;
-  const hours = Math.floor(totalTime / 60);
-  const minutes = Math.floor(totalTime % 60);
+  const totalTimeInMinutes = (destinationDistance / speed) * 60;
+  const hours = Math.floor(totalTimeInMinutes / 60);
+  const minutes = Math.floor(totalTimeInMinutes % 60);
 
   return `${hours} hours and ${minutes} minutes`;
 };
@@ -41,9 +41,7 @@ const travelTime = getTravelTime(travelInformation);
 console.log(travelTime); // 8 hours and 38 minutes
 
 // Series duration of my life ====================================================================
-
 const myLifeDurationYears = 80;
-
 const seriesDurations = [
   {
     title: 'Game of thrones',
@@ -71,28 +69,36 @@ const getLostTImeOnSeries = (seriesList, myLifeDuration) => {
   const totalTimeTook = seriesList.reduce((prev, item) => {
     const totalTimeWatched =
       item.days * 24 * 60 + item.hours * 60 + item.minutes;
-    const partOfLife =
-      Math.round(((totalTimeWatched * 100) / myLifeInMinutes) * 10000) / 10000;
+    const partOfLife = (totalTimeWatched * 100) / myLifeInMinutes;
 
-    console.log(`${item.title} took ${partOfLife}% of my life`);
+    console.log(`${item.title} took ${partOfLife.toFixed(3)}% of my life`);
 
     return prev + partOfLife;
   }, 0);
 
-  console.log(`In total that is ${totalTimeTook}% of my life`);
+  console.log(`In total that is ${totalTimeTook.toFixed(3)}% of my life`);
 };
 
 getLostTImeOnSeries(seriesDurations, myLifeDurationYears);
 
 //  Step 3: Smart-ease - Back to the basics! -----------------------------------------------------------
-
 // NOnoN0nOYes (Note taking app) ================================================================
 
 // Save a note
 const notes = [];
 
 function saveNote(content, id) {
-  notes.push({ content, id });
+  if (typeof content === 'string' && typeof id === 'number') {
+    if (!content || !id) {
+      throw new Error(`Content can't be empty and id can't be 0`);
+    } else {
+      notes.push({ content, id });
+    }
+  } else {
+    throw new Error(
+      `Please, make sure that ${content} is a 'string' and ${id} is a 'number'`
+    );
+  }
 }
 
 saveNote('Pick up groceries', 1);
@@ -102,15 +108,13 @@ console.log(notes); // [{content: 'Pick up groceries', id: 1}, {content: 'Do lau
 
 // Get a note
 const getNote = (id) => {
-  if (id === undefined) {
-    return 'Please enter an id number';
-  } else if (typeof id !== 'number') {
+  if (typeof id !== 'number') {
     return 'The id must be a number';
   }
 
   const yourNote = notes.find((item) => id === item.id);
 
-  if (yourNote === undefined) {
+  if (!yourNote) {
     return "You don't have that note";
   }
 
@@ -123,7 +127,7 @@ console.log(firstNote); // {content: 'Pick up groceries', id: 1}
 // Log out notes
 
 const logOutNotesFormatted = () => {
-  notes.map((item) => {
+  notes.forEach((item) => {
     console.log(
       `The note with id: ${item.id}, has the following note text: ${item.content}`
     );
@@ -146,51 +150,70 @@ deleteNote(2);
 // Adding an activity
 const activities = [];
 const userTimeLimit = 80;
-const date = new Date().toLocaleDateString('en-EU', {
-  weekday: 'short',
-  year: 'numeric',
-  month: 'short',
-  day: 'numeric',
-});
+const date = new Date();
+const formattedDate = `${date.getDay()}/${date.getMonth()}-${date.getFullYear()}`;
 
-const addActivity = (date, activity, duration) => {
-  activities.push({ date, activity, duration });
-};
+const addActivity = (currentDate, activity, duration) => {
+  if (
+    !currentDate ||
+    typeof activity !== 'string' ||
+    !activity ||
+    typeof duration !== 'number'
+  ) {
+    throw new Error(
+      `Please make sure you have entered date(string), activity(string), and duration (number)`
+    );
+  }
 
-addActivity(date, 'Youtube', 30);
-addActivity(date, 'Facebook', 45);
-// addActivity(date, 'Instagram', 30);
-
-// Show my status
-const showStatus = (activitiesList) => {
-  const totalTime = activitiesList.reduce(
+  const totalTimeSpentInApps = activities.reduce(
     (acc, activity) => acc + activity.duration,
     0
   );
 
-  if (totalTime === 0) {
-    console.log('Add some activities before calling showStatus');
+  if (totalTimeSpentInApps >= userTimeLimit) {
+    throw new Error(
+      'You have reached your limit, no more smartphoning for you!'
+    );
+  } else {
+    activities.push({ currentDate, activity, duration });
+  }
+};
+
+addActivity(formattedDate, 'Youtube', 30);
+addActivity(formattedDate, 'Facebook', 45);
+
+// Show my status
+const showStatus = (activitiesList) => {
+  const totalTimeSpentInApps = activitiesList.reduce(
+    (acc, activity) => acc + activity.duration,
+    0
+  );
+
+  if (!totalTimeSpentInApps) {
+    throw new Error('Add some activities before calling showStatus');
     return;
-  } else if (totalTime >= userTimeLimit) {
+  } else if (totalTimeSpentInApps >= userTimeLimit) {
     console.log('You have reached your limit, no more smartphoning for you!');
     return;
   }
 
   console.log(
-    `You have added ${activitiesList.length} activities. They amount to ${totalTime} min. of usage`
+    `You have added ${activitiesList.length} activities. They amount to ${totalTimeSpentInApps} min. of usage`
   );
 };
 
 showStatus(activities);
 
 const mostUsedApp = (activitiesList) => {
-  const maxTimeApp = activitiesList.sort((a, b) => a.duration - b.duration)[
-    activitiesList.length - 1
-  ];
+  if (!activitiesList.length) {
+    throw new Error('Add some activities to know what is the most used app');
+  } else {
+    const maxTimeApp = activitiesList.sort((a, b) => a.duration - b.duration)[
+      activitiesList.length - 1
+    ];
 
-  console.log(
-    `You have spent most of the time in ${JSON.stringify(maxTimeApp)}`
-  );
+    console.log(`You have spent most of the time in ${maxTimeApp.activity}`);
+  }
 };
 
 mostUsedApp(activities);
