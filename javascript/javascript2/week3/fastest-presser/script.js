2a150c35a3f3577a9f0dc287b51b817a82b63194
Rostyslav Zhyvitskyi <rostik94@gmail.com>
2022-06-09 17:39:56 +0200

    count down added, prevented multiple Start Game

--- javascript/javascript2/week3/fastest-presser/script.js
+++ javascript/javascript2/week3/fastest-presser/script.js
@@ -3,6 +3,8 @@ const startBtn = document.getElementById('game-start');
 const playerOneField = document.getElementById('player-one');
 const playerTwoField = document.getElementById('player-two');
 const winner = document.getElementById('winner');
+const countDown = document.getElementById('time-count-down');
+const errorField = document.getElementById('error');
 
 let keySPressedAmount = 0;
 let keyLPressedAmount = 0;
@@ -16,6 +18,8 @@ const renderPoints = (keyS, keyL) => {
 };
 
 const keyboardListener = (e) => {
+  console.log('hello');
+
   if (e.code === 'KeyS') {
     keySPressedAmount++;
   } else if (e.code === 'KeyL') {
@@ -25,33 +29,61 @@ const keyboardListener = (e) => {
   renderPoints(keySPressedAmount, keyLPressedAmount);
 };
 
+let counter;
+
+const makeAfterTimeout = () => {
+  window.removeEventListener('keyup', keyboardListener);
+  if (keySPressedAmount > keyLPressedAmount) {
+    winner.textContent = `Winner Left`;
+  } else if (keySPressedAmount < keyLPressedAmount) {
+    winner.textContent = `Winner Right`;
+  } else {
+    winner.textContent = `It's a draw!`;
+  }
+
+  keySPressedAmount = 0;
+  keyLPressedAmount = 0;
+};
+
+const countDownTimer = (duration) => {
+  let gameDuration = duration - 1;
+
+  countDown.classList.add('active');
+  countDown.textContent = `${duration}`;
+
+  counter = window.setInterval(() => {
+    countDown.textContent = `${gameDuration}`;
+    --gameDuration;
+
+    if (gameDuration < 0) {
+      countDown.textContent = '0';
+      clearInterval(counter);
+      makeAfterTimeout();
+    }
+  }, 1000);
+};
+
 startBtn.addEventListener('click', () => {
   const gameDuration = durationInput.value;
 
   playerOneField.textContent = '';
   playerTwoField.textContent = '';
+  winner.textContent = '';
+  errorField.textContent = '';
+
+  clearInterval(counter); // To prevent multiple Start Game!
+
+  keySPressedAmount = 0; // To prevent multiple Start Game!
+  keyLPressedAmount = 0; // To prevent multiple Start Game!
 
   if (!gameDuration) {
-    return console.log(
-      'Please enter the game duration in seconds, maximum is 10 seconds'
-    );
+    return (errorField.textContent =
+      'Please enter the game duration in seconds, maximum is 10 seconds');
   } else if (gameDuration > 10 || gameDuration <= 0) {
-    return console.log(
-      'Game duration cannot be greater than 10 seconds or less than 1 second'
-    );
+    return (errorField.textContent =
+      'Game duration cannot be greater than 10 seconds or less than 1 second');
   }
 
+  countDownTimer(gameDuration);
   window.addEventListener('keyup', keyboardListener);
-
-  window.setTimeout(() => {
-    window.removeEventListener('keyup', keyboardListener);
-    if (keySPressedAmount > keyLPressedAmount) {
-      winner.textContent = `Winner Left`;
-    } else {
-      winner.textContent = `Winner Right`;
-    }
-
-    keySPressedAmount = 0;
-    keyLPressedAmount = 0;
-  }, gameDuration * 1000);
 });
