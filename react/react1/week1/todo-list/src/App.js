2e716e6ed408600a55fb0eaded1c66e4b0278500
Rostyslav Zhyvitskyi <rostik94@gmail.com>
2022-10-01 16:57:44 +0200

    add header and footer

--- react/react1/week1/todo-list/src/App.js
+++ react/react1/week1/todo-list/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import tasks from './db/tasks.json';
+import Header from './components/layout/header/Header';
+import Footer from './components/layout/footer/Footer';
 import Home from './components/home/Home';
 import Tasks from './components/tasks/Tasks';
 import About from './components/about/About';
 
 function App() {
   return (
-    <>
+    <main>
+      <Header />
       <Routes>
         <Route path={'/'} element={<Home />} />
         <Route path={'/home'} element={<Home />} />
-        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/tasks" element={<Tasks tasks={tasks} />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </>
+      <Footer />
+    </main>
   );
 }
 
