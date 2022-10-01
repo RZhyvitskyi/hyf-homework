e5d3a1bd13b75184fcd34302e199b71c0256763c
Rostyslav Zhyvitskyi <rostik94@gmail.com>
2022-10-01 14:30:27 +0200

    style home

--- react/react1/week1/todo-list/src/components/home/Home.jsx
+++ react/react1/week1/todo-list/src/components/home/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import Container from '../layout/container/Container';
+import styles from './Home.module.css';
 
 const Home = () => {
-  return <Container>hello</Container>;
+  return (
+    <Container>
+      <div className={styles.home}>
+        <h2>This is a home page</h2>
+        <div>
+          <p>Here can be found some main info</p>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default Home;
