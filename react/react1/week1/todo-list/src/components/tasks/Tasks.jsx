f440cc8607faa8cd24bb4c30283e5e3c9833b1ba
Rostyslav Zhyvitskyi <rostik94@gmail.com>
2022-10-01 16:59:53 +0200

    add tasks

---
+++ react/react1/week1/todo-list/src/components/tasks/Tasks.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Container from '../layout/container/Container';
+import './Tasks.css';
+import Task from './task/Task';
+
+const Tasks = ({ tasks }) => {
+  return (
+    <Container>
+      <div className="tasks">
+        <h2 className="tasks__title">My tasks</h2>
+        <ul className="tasks__list">
+          {tasks.map((task) => {
+            return <Task task={task} />;
+          })}
+        </ul>
+      </div>
+    </Container>
+  );
+};
+
+export default Tasks;
