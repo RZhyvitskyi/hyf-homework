fe3f396c0fbe5c6b964d2adb47cb95a4c2892678
Rostyslav Zhyvitskyi <rostik94@gmail.com>
2022-10-01 13:31:49 +0200

    create routes

--- react/react1/week1/todo-list/src/App.js
+++ react/react1/week1/todo-list/src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/home';
+import Tasks from './components/tasks';
+import About from './components/about';
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route path={'/home' && '/'} element={<Home />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
