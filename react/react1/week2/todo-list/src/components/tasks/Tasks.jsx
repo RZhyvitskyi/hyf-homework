56a508178a854584de65b9b3852d85b0a59b8604
Rostyslav Zhyvitskyi <rostik94@gmail.com>
2022-10-15 11:56:32 +0200

    split tasks by status

--- react/react1/week2/todo-list/src/components/tasks/Tasks.jsx
+++ react/react1/week2/todo-list/src/components/tasks/Tasks.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import Container from '../layout/container/Container';
 import './Tasks.css';
-import Task from './task/Task';
+import TaskList from './task-list/TaskList';
 import Form from './form/NewForm';
 
 const Tasks = ({ tasks, addNewTask, deleteTask, changeStatus }) => {
+  const pendingTasks = tasks.filter(({ done }) => !done);
+  const doneTasks = tasks.filter(({ done }) => done);
+
   return (
     <Container>
       <div className="tasks">
         <h2 className="tasks__title">My tasks</h2>
         <Form addNewTask={addNewTask} />
-        <ul className="tasks__list">
-          {tasks.map((task) => {
-            return (
-              <Task
-                task={task}
-                key={task.id}
-                deleteTask={deleteTask}
-                changeStatus={changeStatus}
-              />
-            );
-          })}
-        </ul>
+        <div className="tasks__wrapper">
+          <TaskList
+            tasks={pendingTasks}
+            deleteTask={deleteTask}
+            changeStatus={changeStatus}
+            title="ToDo tasks"
+          />
+          <TaskList
+            tasks={doneTasks}
+            deleteTask={deleteTask}
+            changeStatus={changeStatus}
+            title="Done tasks"
+          />
+        </div>
       </div>
     </Container>
   );
---
+++ react/react1/week2/todo-list/src/components/tasks/task-list/TaskList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './TaskList.css';
+import Task from './../task/Task';
+
+const TaskList = ({ tasks, deleteTask, changeStatus, title }) => {
+  return (
+    <>
+      <h3 className="tasks__status">{title}</h3>
+      <ul className="tasks__list">
+        {tasks.map((task) => {
+          return (
+            <Task
+              task={task}
+              key={task.id}
+              deleteTask={deleteTask}
+              changeStatus={changeStatus}
+            />
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default TaskList;
